720c04c68240ec181a961495c67068e95ffd82fb
michig8620 <barista.course.mg@gmail.com>
2025-04-27 23:24:58 +0300

    changed demo app to python

--- basic-demo-app/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Root endpoint
-router.get('/', (req, res) => {
-  res.send({ message: 'Demo API is running!' });
-});
-
-module.exports = router;
